d5d7b3124b4b9f1c1cad74f09fb2ee658d3805dc
Kenta Xu <43323628+larm60@users.noreply.github.com>
2021-11-15 07:51:40 -0500

    Added odrive component with deadreckoning

--- deadreckoning.py
+++ deadreckoning.py
@@ -1,3 +1,37 @@
+from __future__ import print_function
+
+import odrive
+from odrive.enums import *
+import time
+import math
+
+# Find a connected ODrive (this will block until you connect one)
+print("finding an odrive...")
+my_drive = odrive.find_any()
+
+# -----------------------------------------------------------------------
+# Calibrate motor and wait for it to finish
+print("starting calibration...")
+my_drive.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+while my_drive.axis0.current_state != AXIS_STATE_IDLE:
+    time.sleep(0.1)
+
+my_drive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+
+# ------------------------------------------------------------------------
+
+# Calibrate motor and wait for it to finish
+print("starting calibration...")
+my_drive.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+while my_drive.axis1.current_state != AXIS_STATE_IDLE:
+    time.sleep(0.1)
+
+my_drive.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+# ------------------------------------------------------------------------
+
+my_drive.axis0.controller.input_vel = -20
+my_drive.axis1.controller.input_vel = 20
+
 ticksPerRev = 200
 radius = 120 #in mm
 length = 500 #in mm
@@ -12,7 +46,7 @@ wl = 0
 wr = 0
 
 def computeAngVel():
-    dt_omega = micros() - prevWheelComputeTime
+    dt_omega = time.time()*1000 - prevWheelComputeTime
 
     leftTicks = shadowcount
     rightTicks = shadowcount
@@ -26,14 +60,14 @@ def computeAngVel():
     leftTicksPrev = leftTicks
 	rightTicksPrev = rightTicks
 
-	prevWheelComputeTime = micros()
+	prevWheelComputeTime = time.time()*1000
 
 def computePosition():
     if micros() - prevIntegrationTime > positionComputeInterval:
 		computeAngularVelocities()
 		# Time elapsed after the previous position has been integrated.
 		# change in time is defined as previous - current to prevent round off error.
-		dt_integration = micros() - prevIntegrationTime
+		dt_integration = time.time()*1000 - prevIntegrationTime
 
 		dt = dt_integration / 1000000.0; # convert to seconds
 
@@ -44,8 +78,8 @@ def computePosition():
 		v = (Vr + Vl) / 2.0 #average velocity
 		w = (Vr - Vl) / length #angular velocity
 		# Uses 4th order Runge-Kutta to integrate numerically to find position.
-		xNext = xc + dt * v*(2 + cos(dt*w / 2))*cos(theta + dt * w / 2) / 3
-		yNext = yc + dt * v*(2 + cos(dt*w / 2))*sin(theta + dt * w / 2) / 3
+		xNext = xc + dt * v*(2 + math.cos(dt*w / 2))*math.cos(theta + dt * w / 2) / 3
+		yNext = yc + dt * v*(2 + math.cos(dt*w / 2))*math.sin(theta + dt * w / 2) / 3
 		thetaNext = theta + dt * w
 
 		xc = xNext
@@ -53,6 +87,11 @@ def computePosition():
 		theta = thetaNext
 
 		toRPM = 30 / PI
-		dist = sqrt(xc*xc + yc * yc)
+		dist = math.sqrt(xc*xc + yc * yc)
 
-		prevIntegrationTime = micros()
+		prevIntegrationTime = time.time()*1000
+
+	#loop to keep calculating positions and distances
+	while(1):
+		computePosition()
+		print(xc, yc, dist)
