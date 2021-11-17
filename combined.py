51d7c1af5dfe06c2a881869ed390c207a33fce7c
bdevana3 <57334076+bdevana3@users.noreply.github.com>
2021-11-17 14:37:03 -0500

    Add files via upload

---
+++ combined.py
@@ -0,0 +1,108 @@
+from __future__ import print_function
+
+import odrive
+from odrive.enums import *
+import time
+import math
+
+# Find a connected ODrive (this will block until you connect one)
+print("finding an odrive...")
+my_drive = odrive.find_any()
+
+# -----------------------------------------------------------------------
+# Calibrate motor and wait for it to finish
+print("starting calibration...")
+my_drive.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+while my_drive.axis0.current_state != AXIS_STATE_IDLE:
+	time.sleep(0.1)
+
+my_drive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+
+# ------------------------------------------------------------------------
+
+# Calibrate motor and wait for it to finish
+print("starting calibration...")
+my_drive.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+while my_drive.axis1.current_state != AXIS_STATE_IDLE:
+	time.sleep(0.1)
+
+my_drive.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+# ------------------------------------------------------------------------
+
+ticksPerRev = 200
+radius = 120 #in mm
+length = 500 #in mm
+toRadPerSec = 1000000 * (math.PI * 2) / ticksPerRev
+positionComputeInterval = 100 #in microseconds 
+
+#Global variables
+wl = 0
+wr = 0
+previousWheelComputeTime = 0
+leftTicksPrev = 0
+rightTicksPrev = 0
+previousIntegrationTime = 0
+radius = 120 # mm
+length = 500 # mm
+xc = 0
+yc = 0
+theta = 0
+
+def computeAngVel():
+	global prevWheelComputeTime
+	global leftTicksPrev
+	global rightTicksPrev
+	global wl
+	global wr
+    	dt_omega = time.time()*1000 - prevWheelComputeTime
+    	leftTicks = my_drive.axis1.encoder.shadow_count
+    	rightTicks = my_drive.axis0.encoder.shadow_count
+
+    	changeLeftTicks = leftTicks - leftTicksPrev
+	changeRightTicks = rightTicks - rightTicksPrev
+
+	wl = changeLeftTicks / dt_omega * toRadPerSec
+	wr = changeRightTicks / dt_omega * toRadPerSec
+
+    	leftTicksPrev = leftTicks
+	rightTicksPrev = rightTicks
+
+	prevWheelComputeTime = time.time()*1000
+
+def computePosition():
+	global w1
+	global wr
+	global positionComputeInterval
+	global prevIntegrationTime
+	global radius
+	global length
+	global xc
+	global yc
+	global theta
+    	if time.time()*1000 - prevIntegrationTime > positionComputeInterval:
+		computeAngVel()
+		# Time elapsed after the previous position has been integrated.
+		# change in time is defined as previous - current to prevent round off error.
+		dt_integration = time.time()*1000 - prevIntegrationTime
+
+		dt = dt_integration / 1000000.0; # convert to seconds
+
+		# Dead reckoning equations
+
+		Vl = wl * radius #linear velocity left wheel
+		Vr = wr * radius #linear velocity right wheel
+		v = (Vr + Vl) / 2.0 #average velocity
+		w = (Vr - Vl) / length #angular velocity
+		# Uses 4th order Runge-Kutta to integrate numerically to find position.
+		xNext = xc + dt * v*(2 + math.cos(dt*w / 2))*math.cos(theta + dt * w / 2) / 3
+		yNext = yc + dt * v*(2 + math.cos(dt*w / 2))*math.sin(theta + dt * w / 2) / 3
+		thetaNext = theta + dt * w
+
+		xc = xNext
+		yc = yNext
+		theta = thetaNext
+
+		toRPM = 30 / math.PI
+		dist = math.sqrt(xc*xc + yc * yc)
+
+		prevIntegrationTime = time.time()*1000
